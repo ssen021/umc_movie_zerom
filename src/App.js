7482a1df37cdeb6fa3771f7bb113cca562377e2c
ssen021 <130373143+ssen021@users.noreply.github.com>
2023-10-09 19:38:52 +0900

    mission
    finish

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import movies from './movieDummy';
 function App() {
   return (
     <div className="app-container">
-        {
-          movies.results.map((item) => {
-            return (
-              <Movie key={item.id} title={item.title} poster={item.poster_path} vote_average ={item.vote_average}/>
-            )
-          })
-        }
-      </div>
-    
+      {movies.results.map((item) => (
+        <div key={item.id} className="movie-container">
+          <Movie title={item.title} poster={item.poster_path} vote_average={item.vote_average} overview={item.overview} />
+        </div>
+      ))}
+    </div>
   );
 }
 
 export default App;
+
+const movie_container = document.querySelectorAll('.movie_container');
+const des_container = document.querySelectorAll('.des-container')
+
+movie_container.forEach((movie_container, index) => {
+  movie_container.addEventListener('mouseenter', () => {
+    des_container[index].style.display = 'block';
+  });
+
+  movie_container.addEventListener('mouseleave', () => {
+    des_container[index].style.display = 'none';
+  });
+});
--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,6 +1,12 @@
 function Movie(props) {
     return (
-      <div className="movie-container">
+      <div className="movie_container">
+        <div className="des-container">
+          <div className ="des_section">
+            <div className ="des_title">{props.title}</div>
+            <div className ="overview">{props.overview}</div>
+          </div>
+        </div>
         <div className="movie_section">
           <img className="poster" src ={`https://image.tmdb.org/t/p/w1280/${props.poster}`} alt={props.title}/>
           <div className ="text_section">
