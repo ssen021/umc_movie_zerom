39c2205b27e3d4bc1f5676118dc8dd9bd01ee550
ssen021 <130373143+ssen021@users.noreply.github.com>
2023-10-11 14:08:30 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Movie from './Components/Movie';
 import movies from './movieDummy';
 
 function App() {
+  useEffect(() => {
+    const movieContainers = document.querySelectorAll('.movie-container');
+    const desContainers = document.querySelectorAll('.des-container');
+
+    movieContainers.forEach((movieContainer, index) => {
+      movieContainer.addEventListener('mouseenter', () => {
+        desContainers[index].style.display = 'block';
+      });
+
+      movieContainer.addEventListener('mouseleave', () => {
+        desContainers[index].style.display = 'none';
+      });
+
+      // 마우스를 떠날 때 이벤트 리스너를 정리합니다.
+      return () => {
+        movieContainer.removeEventListener('mouseenter', () => {
+          desContainers[index].style.display = 'block';
+        });
+        movieContainer.removeEventListener('mouseleave', () => {
+          desContainers[index].style.display = 'none';
+        });
+      };
+    });
+  }, []); // 빈 배열을 전달하여 이펙트가 컴포넌트가 마운트될 때만 실행되도록 합니다.
+
   return (
     <div className="app-container">
-      {movies.results.map((item) => (
+      {movies.results.map((item, index) => (
         <div key={item.id} className="movie-container">
           <Movie title={item.title} poster={item.poster_path} vote_average={item.vote_average} overview={item.overview} />
         </div>
@@ -15,16 +40,3 @@ function App() {
 }
 
 export default App;
-
-const movie_container = document.querySelectorAll('.movie_container');
-const des_container = document.querySelectorAll('.des-container')
-
-movie_container.forEach((movie_container, index) => {
-  movie_container.addEventListener('mouseenter', () => {
-    des_container[index].style.display = 'block';
-  });
-
-  movie_container.addEventListener('mouseleave', () => {
-    des_container[index].style.display = 'none';
-  });
-});
